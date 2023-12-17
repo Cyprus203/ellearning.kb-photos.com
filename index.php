bc76003a7950a395ee3993fcb633ac29c55f5669
Cyprus203 <147189660+Cyprus203@users.noreply.github.com>
2023-12-17 22:50:50 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,35 @@
+<html><head>
+
+<title>Hacked By (Philippines Cyber Alliance)</title>
+
+<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet"><link href="https://fonts.googleapis.com/css?family=Bebas+Neue" rel="stylesheet"></head>
+      
+      
+<body bgcolor="black">
+
+<center><font face="Bebas Neue" size="25" color="darkred"> Hacked By Philippines Cyber Alliance </font>
+
+<br><img src="https://i.postimg.cc/tgC8WzSv/385416823-280565358199295-5073321444171139091-n.jpg" width="300" height="300">
+
+<br><br><font face="Roboto" size="4" color="white"> 
+    Hello, China! This message is from Philippines Cyber Alliance<br>
+	MESSAGE TO CHINA GOVERMENT AND ALL THE PEOPLE OF CHINA<br>
+	Many of you are aware of the oppressive acts of the Chinese Government over the South China Sea or West Philippine Sea.
+	Their claims over the said area is nothing but mere assumption and without solid proof. Once again, we must gather our brothers and sisters to stand against this tyranny. Others may think this action does nothing, but so does sitting around while waiting for something to happen.We have a voice, and we will use it to make a stand. We will say no against this oppression for we care about our country. To the Government of China, stop the reclamation, do not put or establish any structure on that location thinking or claiming that you are the owner. Considering that there is no final agreement between both and other parties. "WE DO NOT WANT TO BE BULLIED IN OUR OWN CYBERSPACE"<br><br>
+	 </font>
+
+     <font face="Bebas Neue" size="5" color="darkred"> 
+        </font><p><font face="Bebas Neue" size="5" color="darkred">-We Stand For West Phillipine Sea-
+       
+         </font>
+
+         <font face="Roboto" size="4" color="white"> 
+            </font></p><p><font face="Roboto" size="4" color="white">#OPChina<br><br>
+           
+             </font>
+
+<br>  
+
+
+
+</p></center></body></html>
